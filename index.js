e3f804e8820b30ef6f2f262cfbd8ff3c3bfbeff2
Asraful <iasraful107@gamil.com>
2022-10-29 21:16:14 +0600

    added vercel

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const app = require("./app");
 const connectDatabase = require ("./db/database.js");
 
 // handle uncaught exception 
-// process.on("uncaughtException", (err) => {
-//   console.log(`Error is happing for ${err.message}`)
-//   console.log(`Shutting down the server due to  handle uncaught exception`);
-// });
+process.on("uncaughtException", (err) => {
+  console.log(`Error is happing for ${err.message}`)
+  console.log(`Shutting down the server due to  handle uncaught exception`);
+});
 
 //config 
 
@@ -25,10 +25,10 @@ const server = app.listen(process.env.PORT , () => {
 
 // unHandle Promise rejection
 
-// process.on("unhandledRejection" , (err) => {
-//   console.log(`Shutting down the server for ${err.message}`)
-//   console.log(`Shutting down the server due to  unHandle Promise rejection`)
-//   server.close(() => {
-//       process.exit(1);
-//   });
-// });
+process.on("unhandledRejection" , (err) => {
+  console.log(`Shutting down the server for ${err.message}`)
+  console.log(`Shutting down the server due to  unHandle Promise rejection`)
+  server.close(() => {
+      process.exit(1);
+  });
+});
